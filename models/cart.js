ee6ce2f32c9af0eb74027b639357c0ad4cf549ba
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-07-29 08:44:27 +0000

    update

---
+++ models/cart.js
@@ -0,0 +1,7 @@
+module.exports = class Cart {
+  static addProduct(id) {
+    // Fetch(đọc dữ liệu thông qua Link) giỏ hàng hiện có
+    // Xác nhận trong giỏ hàng => Tìm sản phẩm đã tồn tại
+    // Thêm sản phẩm mới/ tăng số lượng
+  }
+};
